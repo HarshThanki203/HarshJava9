c89ea49d98c5378827bac0280a4d2ac4466b78ae
HarshThanki203 <harshthanki203@gmail.com>
2022-07-20 22:18:33 +0530

    C programs

---
+++ C LANGUAGE ASSIGNMENT/module1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main(){
+          
+          char name[10];
+          int birthdate;
+          int age;
+          char address[30];
+          
+          printf("Enter your name:");
+          scanf("%s",&name);
+          printf("Enter your birthdate:");
+          scanf("%s",&birthdate);
+          printf("Enter your age:");
+          scanf("%d",&age);
+          printf("Enter your address:");
+          scanf("%d",&address);
+          
+          
+
+
+  return 0;
+             
+}
+
+
---
+++ C LANGUAGE ASSIGNMENT/square_cube.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(){
+	
+	int a,b,c;
+	printf(" Square And Cube of Numbers\n");
+	
+	for(a=1;a<=30;a++)
+	{
+		b=a*a;
+		c=a*a*a;
+		printf("%d\t\t%d\t\t%d\n",a,b,c);
+		
+	}
+	
+	return 0;
+	
+}
